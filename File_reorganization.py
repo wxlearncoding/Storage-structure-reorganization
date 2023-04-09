9b2b23abea04b8f3a1fbde70a83176ff0399f0ae
J Wang <33194515+wxlearncoding@users.noreply.github.com>
2023-04-09 19:03:46 +0200

    Add files via upload

---
+++ File_reorganization.py
@@ -0,0 +1,36 @@
+# public in github for paper
+
+import os
+import shutil
+import pydicom
+from pathlib import Path
+
+
+def create_dir(directory):
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+
+
+PathDicom = '.......' # your dicom file path
+
+NewPathDicom = '.......' # your new storage file path
+
+dirs = os.listdir(PathDicom) # read dicom file
+
+for dir in dirs:
+
+    # select information(AcquisitionDate, PatientID, ...) from DICOM header
+    dicomdate = pydicom.read_file(os.path.join(PathDicom, dir)).AcquisitionDate
+    patientid = pydicom.read_file(os.path.join(PathDicom, dir)).PatientID
+
+    # combine new path for DICOM slice
+    newPath = os.path.join(NewPathDicom,patientid,dicomdate)
+
+    # if new path doesn't exist, create this folder
+    if Path(newPath).exists():
+        pass
+    else:
+        create_dir(newPath)
+        # move to new file slice by slice
+        shutil.move(
+            dir, newPath, copy_function = shutil.copytree)  # success
